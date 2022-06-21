ba0418c72a260d780d5ce19627e4e54ba4bfb3c2
lorenzosc <57836636+lorenzosc@users.noreply.github.com>
2022-06-21 16:51:53 -0300

    Update interative_kernel_som.py

--- interative_kernel_som.py
+++ interative_kernel_som.py
@@ -663,64 +663,6 @@ plt.plot(T_fit[:], dS_MxTexp[:,:], label='fit', color=cmap(i+1))
 plt.xlabel('T (K)')
 plt.ylabel('ds (J/kgK)')
 plt.show()
+
 #%% Step 10 - exporting data
-#reading for temperature and experimental specific heat
-
-
-ref_file = open("SpecificHeat.txt","w")
-for i in range(Nline_new):
-
-    if i == 0:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write("T")
-            else:
-                ref_file.write(f"\t{(j-1)}T")
-    else:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write(str(T_fit[i-1]))
-            else:
-                ref_file.write(f"\t{c_fit[i-1,j-1]}")
-
-    ref_file.write('\n')
-ref_file.close()
-
-ref_file = open("TotalEntropy.txt","w")
-for i in range(Nline_new):
-
-    if i == 0:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write("T")
-            else:
-                ref_file.write(f"\t{(j-1)}T")
-    else:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write(str(T_fit[i-1]))
-            else:
-                ref_file.write(f"\t{s[i-1,j-1]}")
-
-    ref_file.write('\n')
-ref_file.close()
-
-ref_file = open("Magnetization.txt","w")
-for i in range(Nline_new):
-
-    if i == 0:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write("T")
-            else:
-                ref_file.write(f"\t{(j-1)}T")
-    else:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write(str(T_fit[i-1]))
-            else:
-                ref_file.write(f"\t{[i-1,j-1]}")
-
-    ref_file.write('\n')
-ref_file.close()
-    
+functions.output_data(Nline_new, Ncol_new, T_fit, c_fit, s, M_fit)
