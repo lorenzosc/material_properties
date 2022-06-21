38455e55f2983852a110834727e696f4240dac83
lorenzosc <57836636+lorenzosc@users.noreply.github.com>
2022-06-21 16:51:30 -0300

    Update interative_kernel_fom.py

--- interative_kernel_fom.py
+++ interative_kernel_fom.py
@@ -6,6 +6,7 @@ from scipy import optimize
 from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
 import scipy.integrate as integrate
+import functions
 
 #%% Inital information: defining ranges of experimental data
 
@@ -640,64 +641,6 @@ plt.scatter(T_exp[:], dS_exp[:,3], label='exp', color='blue')
 plt.xlabel('T (K)')
 plt.ylabel('ds (J/kgK)')
 plt.show()
+
 #%% Step 10 - exporting data
-#reading for temperature and experimental specific heat
-
-
-ref_file = open("SpecificHeat.txt","w")
-for i in range(Nline_new):
-
-    if i == 0:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write("T")
-            else:
-                ref_file.write(f"\t{(j-1)}T")
-    else:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write(str(T_fit[i-1]))
-            else:
-                ref_file.write(f"\t{c_fit[i-1,j-1]}")
-
-    ref_file.write('\n')
-ref_file.close()
-
-ref_file = open("TotalEntropy.txt","w")
-for i in range(Nline_new):
-
-    if i == 0:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write("T")
-            else:
-                ref_file.write(f"\t{(j-1)}T")
-    else:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write(str(T_fit[i-1]))
-            else:
-                ref_file.write(f"\t{s[i-1,j-1]}")
-
-    ref_file.write('\n')
-ref_file.close()
-
-ref_file = open("Magnetization.txt","w")
-for i in range(Nline_new):
-
-    if i == 0:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write("T")
-            else:
-                ref_file.write(f"\t{(j-1)}T")
-    else:
-        for j in range(Ncol_new+1):
-            if j == 0:
-                ref_file.write(str(T_fit[i-1]))
-            else:
-                ref_file.write(f"\t{[i-1,j-1]}")
-
-    ref_file.write('\n')
-ref_file.close()
-    
+functions.output_data(Nline_new, Ncol_new, T_fit, c_fit, s, M_fit)
